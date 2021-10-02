5ca0233700617114f8ac1c5abcfbd74a6ca837cf
Astrale <jules180798@yahoo.fr>
2021-10-02 14:59:19 +0200

    elm powered now, css wont work anymore

---
+++ src/Main.elm
@@ -0,0 +1,27 @@
+
+module Main exposing (..)
+
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (href)
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+init =
+  div [] []
+
+update msg msg2 =
+  div [] []
+
+
+view x =
+  div []
+    [ h1 [] [ text "Ce site internet" ]
+      , text "Ce site internet est une plateforme avec plusieurs simulations de physique chimie."
+      , h2 [] [text "Niveau 4e"]
+      , a [ href "assets/4e/cours/4e.zip" ] [ text "Télécharger le cours"]
+      , li [] [ text "" ]
+      , a [ href "assets/4e/chimie/godot.html" ] [ text "Équilibrer des équations chimiques"]
+      , li [] [ text "" ]
+      , a [ href "assets/4e/act1/godot_html.html" ] [ text "Activité 1: Que contient notre univers?"]
+    ]
