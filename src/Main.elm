7db4b187ebdbc4922e825c77c0067ebaa34540aa
Astrale <jules180798@yahoo.fr>
2022-01-06 22:00:01 +0100

    ajoute les cours et enlève code inutile

--- src/Main.elm
+++ src/Main.elm
@@ -30,15 +30,13 @@ main =
                         , Element.centerX
                         ]
                         (text "chapitre 1")
-                    , make_ref_list chap1_6e activite6echap1
-                        |> view_ref_tuple_simple
+                    , view_ref_tuple_simple activite6echap1
                     , el
                         [ Element.Font.size 25
                         , Element.centerX
                         ]
                         (text "chapitre 2")
-                    , make_ref_list chap2_6e activite6echap2
-                        |> view_ref_tuple_simple
+                    , view_ref_tuple_simple activite6echap2
                     ]
                 , Element.column [ Element.alignTop ]
                     [ -- el [ Element.padding 10 ] none,
@@ -52,103 +50,59 @@ main =
                         , Element.centerX
                         ]
                         (text "chapitre 1")
-                    , make_ref_list chap1_5e activite5echap1
-                        |> view_ref_tuple_simple
+                    , view_ref_tuple_simple activite5echap1
                     , el
                         [ Element.Font.size 25
                         , Element.centerX
                         ]
                         (text "chapitre 2")
-                    , make_ref_list chap2_5e activite5echap2
-                        |> view_ref_tuple_simple
+                    , view_ref_tuple_simple activite5echap2
                     ]
                 ]
             ]
         )
 
 
-chap1_5e : String
-chap1_5e =
-    "./assets/5e/chap1/activités/"
-
-
-chap2_5e : String
-chap2_5e =
-    "./assets/5e/chap2/activités/"
-
-
-chap1_6e : String
-chap1_6e =
-    "./assets/6e/chap1/activités/"
-
-
-chap2_6e : String
-chap2_6e =
-    "./assets/6e/chap2/activités/"
-
-
 activite5echap1 =
-    [ ( "activité 1", "activité1/act1.pdf" )
-    , ( "activité 2", "activité2/act2.pdf" )
-    , ( "activité 3", "activité3/act3.pdf" )
-    , ( "activité 4", "activité4/act4.pdf" )
-    , ( "activité 5", "activité5/act5.pdf" )
+    [ ( "activité 1", "./assets/5e/chap1/activités/activité1/act1.pdf" )
+    , ( "activité 2", "./assets/5e/chap1/activités/activité2/act2.pdf" )
+    , ( "activité 3", "./assets/5e/chap1/activités/activité3/act3.pdf" )
+    , ( "activité 4", "./assets/5e/chap1/activités/activité4/act4.pdf" )
+    , ( "activité 5", "./assets/5e/chap1/activités/activité5/act5.pdf" )
+    , ( "Cours", "./assets/5e/chap1/cours.pdf" )
     ]
 
 
 activite5echap2 =
-    [ ( "activité 1", "activité1/act1.pdf" )
-    , ( "activité 2", "activité2/act2.pdf" )
-    , ( "activité 3", "activité3/act3.pdf" )
-    , ( "activité 4", "activité4/act4.pdf" )
-    , ( "tache complexe", "tache_complexe/tache_complexe.pdf" )
+    [ ( "activité 1", "./assets/5e/chap2/activités/activité1/act1.pdf" )
+    , ( "activité 2", "./assets/5e/chap2/activités/activité2/act2.pdf" )
+    , ( "activité 3", "./assets/5e/chap2/activités/activité3/act3.pdf" )
+    , ( "activité 4", "./assets/5e/chap2/activités/activité4/act4.pdf" )
+    , ( "tache complexe", "./assets/5e/chap2/activités/tache_complexe/tache_complexe.pdf" )
+    , ( "Cours", "./assets/5e/chap2/cours.pdf" )
     ]
 
 
 activite6echap1 =
-    [ ( "activité 1", "activité1/act1.pdf" )
-    , ( "activité 2", "activité2/act2.pdf" )
-    , ( "activité 3", "activité3/act3.pdf" )
-    , ( "activité 4", "activité4/act4.pdf" )
+    [ ( "activité 1", "./assets/6e/chap1/activités/activité1/act1.pdf" )
+    , ( "activité 2", "./assets/6e/chap1/activités/activité2/act2.pdf" )
+    , ( "activité 3", "./assets/6e/chap1/activités/activité3/act3.pdf" )
+    , ( "activité 4", "./assets/6e/chap1/activités/activité4/act4.pdf" )
+    , ( "Cours", "./assets/6e/chap1/cours.pdf" )
     ]
 
 
 activite6echap2 =
-    [ ( "activité 1", "activité1/act1.pdf" )
-    , ( "activité 2", "activité2/act2.pdf" )
-    , ( "activité 3", "activité3/act3.pdf" )
+    [ ( "activité 1", "./assets/6e/chap2/activités/activité1/act1.pdf" )
+    , ( "activité 2", "./assets/6e/chap2/activités/activité2/act2.pdf" )
+    , ( "activité 3", "./assets/6e/chap2/activités/activité3/act3.pdf" )
+    , ( "Cours", "./assets/6e/chap2/cours.pdf" )
     ]
-
-
-add_prefix : String -> ( String, String ) -> ( String, String )
-add_prefix prefix cont =
-    ( Tuple.first cont, prefix ++ Tuple.second cont )
-
-
-make_ref_list chapitre activites =
-    activites
-        |> List.map (add_prefix chapitre)
-
-
-
--- view_ref_list_simple : List String -> el
-
-
-view_ref_list_simple list =
-    List.map string_to_link list |> Element.column [ Element.padding 10 ]
 
 
 view_ref_tuple_simple : List ( String, String ) -> Element.Element msg
 view_ref_tuple_simple list =
     List.map tuple_to_link list |> Element.column [ Element.padding 10 ]
-
-
-string_to_link : String -> Element.Element msg
-string_to_link str =
-    download [ Element.Font.color (Element.rgb 0.1 0.1 1) ]
-        { url = str
-        , label = text str
-        }
 
 
 tuple_to_link : ( String, String ) -> Element.Element msg
@@ -157,12 +111,3 @@ tuple_to_link tuple =
         { url = Tuple.second tuple
         , label = text (Tuple.first tuple)
         }
-
-
-
--- content : el
-
-
-content =
-    make_ref_list chap1_5e activite5echap1
-        |> view_ref_tuple_simple
